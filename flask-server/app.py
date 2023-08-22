be45569eb4d4779e1622a86f61d75775e41e80b6
Emyr298 <emi.ru298@gmail.com>
2023-08-22 13:35:15 +0700

    s

---
+++ flask-server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, redirect, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def get_csrf_html():
+    return render_template('tbdxss.html'), 200
+    
+@app.route('/error')
+def get_error():
+    return 'Error', 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
