4de6ddd6da1b1f52422e749c80f33c9a7bfde183
Emyr298 <emi.ru298@gmail.com>
2023-08-22 20:21:27 +0700

    update

--- flask-server/app.py
+++ flask-server/app.py
@@ -5,6 +5,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+def validate_filename(filename: str):
+    banned = ['..', '/', '~']
+    for substring in banned:
+        if substring in filename:
+            return False
+    return True
+
 @app.route('/')
 def get_csrf_html():
     return render_template('tbdxss.html'), 200
@@ -13,6 +20,14 @@ def get_csrf_html():
 def get_error():
     time.sleep(5)
     return 'Error', 404
+
+@app.route('/static/<filename>')
+def get_static(filename):
+    if not validate_filename(filename):
+        return 'Invalid filename', 400
+    with open(filename, 'r') as file:
+        content = file.read()
+    return content, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
