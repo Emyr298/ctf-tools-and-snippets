064ddafa8ce21105be4ba1a48c61deff09cec0a3
Emyr298 <emi.ru298@gmail.com>
2024-09-22 23:28:54 +0700

    feat: add flag listener

--- listeners/flag-listener/server.py
+++ listeners/flag-listener/server.py
@@ -2,6 +2,9 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 
+FARMER_URL = "http://localhost:8000"
+FARMER_PASSWORD = "insert password here"
+
 app = Flask(__name__)
 CORS(app)
 
@@ -9,8 +12,8 @@ CORS(app)
 def index():
     flag = request.args.get("flag")
     real_flag = "GEMASTIK{" + flag[len("GEMASTIK"):] + "}"
-    requests.post("http://localhost:8000/api/post_flags", headers={
-        "Authorization": "insert password here",
+    requests.post(f"{FARMER_URL}/api/post_flags", headers={
+        "Authorization": FARMER_PASSWORD,
     }, json=[
         {
             "flag": real_flag,
